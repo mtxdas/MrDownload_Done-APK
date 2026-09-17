c955425bd82f3c066100461066451da8d43624e2
Mithun Das <163460887+mtxdas@users.noreply.github.com>
2026-09-17 21:17:17 +0530

    Remove custom minifier from metro.config.js
    Removed custom minifier path from metro config.

--- metro.config.js
+++ metro.config.js
@@ -2,9 +2,4 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
-config.transformer = {
-  ...config.transformer,
-  minifierPath: 'metro-minify-terser',
-};
-
 module.exports = config;
